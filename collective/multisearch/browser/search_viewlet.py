742bbb10398c51cd33691f3ce6343bffe95eda11
v.pretre <v.pretre@35850d7f-6862-7b4e-9bfd-3656bc855f3a>
2013-01-07 15:49:26 +0000

    Add a copy of the default searchbox viewlet pointing to @@multisearch instead of @@search Added missing template for the search portlet
    git-svn-id: https://svn.vmm.be/svn/Webteam/buildouts/kennis.vmm.be/branches/plone_4/src/collective.multisearch@3614 35850d7f-6862-7b4e-9bfd-3656bc855f3a

---
+++ collective/multisearch/browser/search_viewlet.py
@@ -0,0 +1,6 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.layout.viewlets import common
+
+class SearchBoxViewlet(common.SearchBoxViewlet):
+    index = ViewPageTemplateFile('templates/searchbox_viewlet.pt')

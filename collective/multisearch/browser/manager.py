6a857d2ee897fc012432a46e79861064da86c88b
v.pretre <v.pretre@35850d7f-6862-7b4e-9bfd-3656bc855f3a>
2013-01-21 10:17:30 +0000

    COlumn value is now saved
    git-svn-id: https://svn.vmm.be/svn/Webteam/buildouts/kennis.vmm.be/branches/plone_4/src/collective.multisearch@3688 35850d7f-6862-7b4e-9bfd-3656bc855f3a

--- collective/multisearch/browser/manager.py
+++ collective/multisearch/browser/manager.py
@@ -6,21 +6,27 @@ from zope.component import queryMultiAdapter
 from zope.interface import implements
 from zope.interface import Interface
 
-from plone.app.portlets.browser.manage  import ManageContextualPortlets
+from plone.app.portlets.browser.manage import ManageContextualPortlets
 
-from plone.app.portlets.browser.editmanager import ContextualEditPortletManagerRenderer
+from Products.statusmessages.interfaces import IStatusMessage
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.portlets.browser.editmanager import ContextualEditPortletManagerRenderer
 
 from plone.portlets.interfaces import IPortletManager
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.portlets.browser.interfaces import IManageContextualPortletsView
 
 from collective.multisearch.config import COLUMN_COUNT
+from collective.multisearch.config import DEFAULT_COLUMN
+
 from collective.multisearch.utils import get_column_number
+from collective.multisearch.utils import set_column_number
+from collective.multisearch import MultiSearchMessageFactory as _
 from collective.multisearch.browser.interfaces import IMultisearchPortletManager
 from collective.multisearch.browser.interfaces import IMultiSearchPortletManagerRenderer
 
 from plone.portlets.interfaces import IPortletManagerRenderer
+
 
 class MultiSearchContextualEditPortletManagerRenderer(ContextualEditPortletManagerRenderer):
     adapts(Interface, IDefaultBrowserLayer, IManageContextualPortletsView, IMultisearchPortletManager)
@@ -40,3 +46,22 @@ class MultiSearchManagerContextualPortlets(ManageContextualPortlets):
 
         editmanager.update()
         return editmanager.render()
+
+    def __call__(self):
+        if self.request.get('REQUEST_METHOD') != 'POST':
+            return self.index()
+
+        try:
+            column_count = int(self.request.form.get('column_count',
+                                                     DEFAULT_COLUMN))
+            set_column_number(self.context, column_count)
+            IStatusMessage(self.request).addStatusMessage(
+                _('Properties updated'),
+                'info')
+            return self.request.response.redirect('@@multisearch')
+        except:
+            IStatusMessage(self.request).addStatusMessage(
+                _('Invalid value for column number'),
+                'error')
+            
+        return self.index()
--- collective/multisearch/utils.py
+++ collective/multisearch/utils.py
@@ -2,8 +2,7 @@ from Products.CMFCore.utils import getToolByName
 
 from collective.multisearch.config import DEFAULT_COLUMN
 
-
-def get_column_number(context):
+def get_ms_props(context):
     pprops = getToolByName(context,
                            'portal_properties',
                            None)
@@ -13,9 +12,18 @@ def get_column_number(context):
     if 'multisearch_properties' not in pprops.keys():
         pprops.addPropertySheet('multisearch_properties')
 
-    ms_props = pprops.get('multisearch_properties')
+    return pprops.get('multisearch_properties')
 
+
+def get_column_number(context):
+    ms_props = get_ms_props(context)
     if not ms_props.hasProperty('column_number'):
         ms_props._setProperty('column_number', DEFAULT_COLUMN, 'int')
 
-    return ms_props.get('column_number', DEFAULT_COLUMN)
+    return ms_props.column_number
+
+
+def set_column_number(context, value):
+    ms_props = get_ms_props(context)
+    ms_props._setPropValue('column_number', value)
+    

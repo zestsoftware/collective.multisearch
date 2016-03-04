958fd858eb0ee34f3c0741f171ec3e3865335d0a
Maurits van Rees <maurits@vanrees.org>
2016-03-04 19:07:05 +0100

    Reindented setup.py

--- setup.py
+++ setup.py
@@ -2,38 +2,39 @@ from setuptools import setup, find_packages
 
 version = '1.0.4.dev0'
 
-setup(name='collective.multisearch',
-      version=version,
-      description="Portlet based display for the Plone search page",
-      long_description=(open("README.txt").read() + "\n\n" +
-                        open("CHANGES.rst").read()),
-      # Get more strings from
-      # https://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-      ],
-      keywords='',
-      author='Zest Software',
-      author_email='info@zestsoftware.nl',
-      url='https://github.com/zestsoftware/collective.multisearch',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'five.grok',
-          'feedparser',
-      ],
-      extras_require={
-          'test': [
-              'Products.PloneTestCase',
-          ]
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='collective.multisearch',
+    version=version,
+    description="Portlet based display for the Plone search page",
+    long_description=(open("README.txt").read() + "\n\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='Zest Software',
+    author_email='info@zestsoftware.nl',
+    url='https://github.com/zestsoftware/collective.multisearch',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'five.grok',
+        'feedparser',
+    ],
+    extras_require={
+        'test': [
+            'Products.PloneTestCase',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )

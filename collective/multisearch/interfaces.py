b4b4b05bdd4faffa3a568a03f1477b96c4be3147
Maurits van Rees <maurits@vanrees.org>
2022-05-06 16:15:36 +0200

    Add registry settings.

--- collective/multisearch/interfaces.py
+++ collective/multisearch/interfaces.py
@@ -1,5 +1,19 @@
+from collective.multisearch import MultiSearchMessageFactory as _
+from collective.multisearch.config import DEFAULT_COLUMN
+from zope import schema
 from zope.interface import Interface
 
 
 class IAddOnInstalled(Interface):
     """A layer specific for this add-on product."""
+
+
+class IMultisearchSettings(Interface):
+
+    column_number = schema.Int(
+        title=_("Number of columns"),
+        description=_("Number of columns shown on the multisearch page."),
+        default=DEFAULT_COLUMN,
+        missing_value=DEFAULT_COLUMN,
+        required=True,
+    )
